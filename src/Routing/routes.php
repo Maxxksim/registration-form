b6a6b46ff9d63b86384839623f40448c9ccab87b
Maksym <makximchagin@gmail.com>
2026-09-15 13:16:52 +0300

    feat: new routes

--- src/Routing/routes.php
+++ src/Routing/routes.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use Src\app\Controllers\HomeController;
+use Src\app\Controllers\RegisterController;
 use Src\Routing\Route;
 
 return [
-
+    Route::get('/', [HomeController::class, 'index']),
+    Route::post('/register', [RegisterController::class, 'register'])
 ];
