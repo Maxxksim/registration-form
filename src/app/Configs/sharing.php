df2746ed77ab94438799e478f5937853fd34164f
Maksym <makximchagin@gmail.com>
2026-09-21 20:03:17 +0300

    chore: change url for sharing

--- src/app/Configs/sharing.php
+++ src/app/Configs/sharing.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'url' => 'chahin.albedo.dev',
+    'url' => 'https://chahin.albedo.dev/',
     'text' => 'Conference in 7060 Hollywood Blvd, Los Angeles, CA. I have just registered for the conference!',
 ];
