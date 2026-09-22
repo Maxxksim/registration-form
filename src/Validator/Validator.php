359b41d57ab42403cd5e109a5b5afb773be2fd05
Maksym <makximchagin@gmail.com>
2026-09-22 14:45:43 +0300

    fix: rule phone work conversely

--- src/Validator/Validator.php
+++ src/Validator/Validator.php
@@ -58,7 +58,7 @@ class Validator
                     'unique' => $this->validateUnique($data[$field], $field) ? 'unique' : null,
                     'email' => !filter_var($data[$field], FILTER_VALIDATE_EMAIL) ? 'email' : null,
                     'int' => !ctype_digit($data[$field]) ? 'int' : null,
-                    'phone' => ($result = $this->validatePhoneNumber($data[$field], $data['country']))['result'] ? ['phone', $result] : null,
+                    'phone' => !($result = $this->validatePhoneNumber($data[$field], $data['country']))['result'] ? ['phone', $result] : null,
                     'date' => !$this->validateDate($data[$field]) ? 'date' : null,
                     'birthdate' => !$this->validateBirthdate($data[$field]) ? 'birthdate' : null,
                 };
