714c6cea3d1aa753a216538a570a81c87f7db1bb
Maksym <makximchagin@gmail.com>
2026-09-22 13:05:32 +0300

    fix: add more chars to phone field

--- src/Database/migrations/1_members.sql
+++ src/Database/migrations/1_members.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS members
     birthdate      DATE         NOT NULL,
     report_subject VARCHAR(255)         NOT NULL,
     country        VARCHAR(255) NOT NULL,
-    phone          VARCHAR(17)  NOT NULL,
+    phone          VARCHAR(20)  NOT NULL,
     email          VARCHAR(255) NOT NULL UNIQUE,
     company        VARCHAR(255),
     position       VARCHAR(255),
