85de94c09f1a81c5b7925b9ec31cc3bd8f9692ef
Maksym <makximchagin@gmail.com>
2026-09-23 10:28:02 +0300

    chore: off autocomplete

--- src/app/Views/Components/step1.php
+++ src/app/Views/Components/step1.php
@@ -18,26 +18,26 @@ declare(strict_types=1);
 <form method="post"
       class="max-w-md mx-auto flex flex-col gap-4 p-4"
       id="step1-form">
-    <input type="hidden" name="csrf_token" id="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
+    <input type="hidden" autocomplete="off" name="csrf_token" id="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
     <div class="relative z-0 w-full group"><p class="">
             Fields marked with <span class="text-red-500">*</span> are required.
         </p></div>
     <div class="relative z-0 w-full group"><label class="after:ml-1 after:text-red-500 after:content-['*']">First
             Name</label>
-        <input type="text" value="<?= htmlspecialchars($steps['data']['first_name'] ?? '') ?>"
+        <input type="text" autocomplete="off" value="<?= htmlspecialchars($steps['data']['first_name'] ?? '') ?>"
                required name="first_name" class="border rounded-md w-full px-3 py-2">
         <p class="hidden text-red-700" id="first_name_error"></p>
     </div>
     <div class="relative z-0 w-full group"><label class="after:ml-1 after:text-red-500 after:content-['*']">Last
             Name</label>
-        <input type="text" value="<?= htmlspecialchars($steps['data']['last_name'] ?? '') ?>"
+        <input type="text" autocomplete="off" value="<?= htmlspecialchars($steps['data']['last_name'] ?? '') ?>"
                name="last_name"
                required maxlength="100" class="border rounded-md w-full px-3 py-2">
         <p class="hidden text-red-700" id="last_name_error"></p>
     </div>
     <div class="relative z-0 w-full group"><label
                 class="after:ml-1 after:text-red-500 after:content-['*']">Birthdate</label>
-        <input type="date" value="<?= htmlspecialchars($steps['data']['birthdate'] ?? '') ?>"
+        <input type="date" autocomplete="off" value="<?= htmlspecialchars($steps['data']['birthdate'] ?? '') ?>"
                name="birthdate"
                id="birthdate"
                required class="border rounded-md w-full px-3 py-2 ">
@@ -45,13 +45,13 @@ declare(strict_types=1);
     </div>
     <div class="relative z-0 w-full group"><label class="after:ml-1 after:text-red-500 after:content-['*']">Report
             subject</label>
-        <input type="text" value="<?= htmlspecialchars($steps['data']['report_subject'] ?? '') ?>"
+        <input type="text" autocomplete="off" value="<?= htmlspecialchars($steps['data']['report_subject'] ?? '') ?>"
                name="report_subject" required maxlength="255" class="border rounded-md w-full px-3 py-2">
         <p class="hidden text-red-700" id="report_subject_error"></p>
     </div>
     <div class="relative z-0 w-full group"><label
                 class="after:ml-1 after:text-red-500 after:content-['*'] ">Country</label>
-        <select name="country" id="country" class="border rounded-md w-full px-3 py-2">
+        <select name="country" autocomplete="off" id="country" class="border rounded-md w-full px-3 py-2">
             <option value="" <?= empty($steps['data']['country']) ? 'selected' : '' ?> disabled>Select a country
             </option>
             <?php foreach ($countries as $country) { ?>
@@ -66,7 +66,7 @@ declare(strict_types=1);
     </div>
     <div class="relative z-0 w-full group"><label
                 class="after:ml-1 after:text-red-500 after:content-['*']">Phone</label>
-        <input type="tel" id="phone"
+        <input type="tel" id="phone" autocomplete="off"
                name="phone"
                required
                value="<?= htmlspecialchars($steps['data']['phone'] ?? '') ?>"
@@ -76,7 +76,7 @@ declare(strict_types=1);
     <div class="relative z-0 w-full group"><label
                 class="after:ml-1 after:text-red-500 after:content-['*']">Email</label>
         <input type="email" value="<?= htmlspecialchars($steps['data']['email'] ?? '') ?>" name="email"
-               required class="border rounded-md w-full px-3 py-2">
+               required autocomplete="off" class="border rounded-md w-full px-3 py-2">
         <p class="hidden text-red-700" id="email_error"></p>
     </div>
     <button id="stepOneBtn" type="button" class="border rounded-md w-full px-3 py-2 hover:bg-gray-300">Next</button>
--- src/app/Views/Components/step2.php
+++ src/app/Views/Components/step2.php
@@ -3,7 +3,7 @@
     <input type="hidden" name="csrf_token" id="csrf_token" value="<?= $_SESSION['csrf_token'] ?>">
     <div class="flex flex-col gap-1">
         <label for="company" class="text-sm font-medium">Company</label>
-        <input type="text" id="company" name="company"
+        <input type="text" autocomplete="off" id="company" name="company"
                value="<?= htmlspecialchars($steps['data']['company'] ?? '') ?>"
                class="border rounded-md w-full px-3 py-2">
         <p class="hidden text-red-700 text-sm" id="company_error"></p>
@@ -11,7 +11,7 @@
 
     <div class="flex flex-col gap-1">
         <label for="position" class="text-sm font-medium">Position</label>
-        <input type="text" id="position" name="position"
+        <input type="text" autocomplete="off" id="position" name="position"
                value="<?= htmlspecialchars($steps['data']['position'] ?? '') ?>"
                class="border rounded-md w-full px-3 py-2">
         <p class="hidden text-red-700 text-sm" id="position_error"></p>
@@ -19,14 +19,14 @@
 
     <div class="flex flex-col gap-1">
         <label for="about_me" class="text-sm font-medium">About me</label>
-        <textarea name="about_me" id="about_me" rows="5"
+        <textarea name="about_me" autocomplete="off" id="about_me" rows="5"
                   class="border rounded-md w-full px-3 py-2"><?= htmlspecialchars($steps['data']['about_me'] ?? '') ?></textarea>
         <p class="hidden text-red-700 text-sm" id="about_me_error"></p>
     </div>
     <label for="photo" class="text-sm font-medium">Photo</label>
     <div class="flex  flex-col gap-1">
         <div class="relative">
-            <input type="file" id="photo" name="photo" accept="image/png, image/jpeg, image/webp"
+            <input type="file" autocomplete="off" id="photo" name="photo" accept="image/png, image/jpeg, image/webp"
                    class="border rounded-md w-full px-3 py-2  pr-12 hover:file:bg-gray-300">
             <button type="button" id="cancel"
                     class="hidden absolute right-0.5 rounded  px-3 py-2 hover:bg">✕
