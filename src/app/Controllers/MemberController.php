27e526116c087a3ef59bd347936c7d4d77ede8bf
Maksym <makximchagin@gmail.com>
2026-09-25 09:19:34 +0300

    fix: user can do steps + new member if email is not the same which was before

--- src/app/Controllers/MemberController.php
+++ src/app/Controllers/MemberController.php
@@ -7,6 +7,7 @@ namespace Src\app\Controllers;
 use Src\app\Models\Member;
 use Src\app\Requests\MemberStepOneRequest;
 use Src\app\Requests\MemberStepTwoRequest;
+use Src\app\Requests\UpdateMemberStepOneRequest;
 use Src\Controller\Controller;
 
 class MemberController extends Controller
@@ -21,26 +22,38 @@ class MemberController extends Controller
 
     public function backStep(): void
     {
-        if (!isset($_SESSION['steps']['current']) && $_SESSION['steps']['current'] !== 'step2') {
+        if (!isset($_SESSION['steps']['current']) || $_SESSION['steps']['current'] !== 'step2') {
             http_response_code(403);
             echo json_encode(['error' => 'You must be on the step 2']);
             exit();
         }
+        $_SESSION['steps']['current'] = 'step1';
         http_response_code(200);
         echo json_encode(['backStep' => 'step1']);
     }
 
     public function stepOne(): void
     {
-        if (!isset($_SESSION['steps']['current']) && $_SESSION['steps']['current'] !== 'step1') {
+        if (!isset($_SESSION['steps']['current']) || $_SESSION['steps']['current'] !== 'step1') {
             http_response_code(403);
             echo json_encode(['error' => 'You must be on the step 1']);
             exit();
         }
-        $memberRequest = new MemberStepOneRequest($this->request->validator, $this->request->get, $this->request->post, $this->request->files, $this->request->server);
         $member = new Member($this->db, $this->phoneNumberUtil, $this->countries);
+
+        $savedEmail = $_SESSION['steps']['data']['email'] ?? null;
+        $newEmail = $this->request->getData()['email'] ?? '';
+        $normalizeNewEmail = mb_strtolower(mb_trim($newEmail));
+
+        if ($savedEmail && $savedEmail === $normalizeNewEmail) {
+            $memberRequest = new UpdateMemberStepOneRequest($this->request->validator, $this->request->get, $this->request->post, $this->request->files, $this->request->server);
+            $isUpdate = true;
+        } else {
+            $memberRequest = new MemberStepOneRequest($this->request->validator, $this->request->get, $this->request->post, $this->request->files, $this->request->server);
+            $isUpdate = false;
+        }
+
         $result = $memberRequest->validated();
-        $validatedData = $result['validatedData'];
 
         if ($errors = $result['errors']) {
             http_response_code(422);
@@ -48,24 +61,24 @@ class MemberController extends Controller
             exit();
         }
 
-        $savedEmail = $_SESSION['steps']['data']['email'] ?? null;
-        $newEmail = $validatedData['email'] ?? null;
+        $validatedData = $result['validatedData'];
 
-        if ($savedEmail && $savedEmail === $newEmail) {
+        if ($isUpdate) {
             $savedData = $member->update('email', $_SESSION['steps']['data']['email'], $validatedData);
             $_SESSION['steps']['data'] = array_merge($_SESSION['steps']['data'], $savedData);
         } else {
             $savedData = $member->create($validatedData);
-            $_SESSION['steps']['data'] = array_merge($savedData);
+            $_SESSION['steps']['data'] = $savedData;
         }
-        $_SESSION['steps']['current'] = ['step2'];
+        $_SESSION['steps']['current'] = 'step2';
+
         echo json_encode(['nextStep' => 'step2']);
         exit();
     }
 
     public function stepTwo(): void
     {
-        if (!isset($_SESSION['steps']['current']) && $_SESSION['steps']['current'] !== 'step2') {
+        if (!isset($_SESSION['steps']['current']) || $_SESSION['steps']['current'] !== 'step2') {
             http_response_code(403);
             echo json_encode(['error' => 'You must be on the step 2']);
             exit();
@@ -90,7 +103,7 @@ class MemberController extends Controller
         $member->update('email', $_SESSION['steps']['data']['email'], $validatedData);
 
         $countMembers = count($member->getMembers());
-
+        session_destroy();
         echo json_encode(['nextStep' => 'stepThanks', 'countMembers' => $countMembers]);
         exit();
     }
---
+++ src/app/Requests/UpdateMemberStepOneRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Src\app\Requests;
+
+class UpdateMemberStepOneRequest extends MemberStepOneRequest
+{
+    public function rules(): array
+    {
+        return array_merge(parent::rules(), [
+            'email' => ['required', 'email', 'max:255'],
+        ]);
+    }
+}
